24faa08fa22bb190f4b05924e53e79424df63e0c
416rehman <416rehman@ahmadz.ai>
2024-05-29 00:02:03 -0400

    Added UI

---
+++ Source/Mythic/UI/MythicActivatableWidget.cpp
@@ -0,0 +1,39 @@
+﻿// 
+#include "MythicActivatableWidget.h"
+#include "Input/CommonUIInputTypes.h"
+
+void UMythicActivatableWidget::NativeDestruct() {
+    for (FUIActionBindingHandle Handle : BindingHandles) {
+        if (Handle.IsValid()) {
+            Handle.Unregister();
+        }
+    }
+    BindingHandles.Empty();
+
+    Super::NativeDestruct();
+}
+
+void UMythicActivatableWidget::RegisterBinding(FDataTableRowHandle InputAction, const FInputActionExecutedDelegate &Callback,
+                                               FInputActionBindingHandle &BindingHandle) {
+    FBindUIActionArgs BindArgs(InputAction, FSimpleDelegate::CreateLambda([InputAction, Callback]() {
+        Callback.ExecuteIfBound(InputAction.RowName);
+    }));
+    BindArgs.bDisplayInActionBar = true;
+
+    BindingHandle.Handle = RegisterUIActionBinding(BindArgs);
+    BindingHandles.Add(BindingHandle.Handle);
+}
+
+void UMythicActivatableWidget::UnregisterBinding(FInputActionBindingHandle BindingHandle) {
+    if (BindingHandle.Handle.IsValid()) {
+        BindingHandle.Handle.Unregister();
+        BindingHandles.Remove(BindingHandle.Handle);
+    }
+}
+
+void UMythicActivatableWidget::UnregisterAllBindings() {
+    for (FUIActionBindingHandle Handle : BindingHandles) {
+        Handle.Unregister();
+    }
+    BindingHandles.Empty();
+}
---
+++ Source/Mythic/UI/MythicActivatableWidget.h
@@ -0,0 +1,43 @@
+﻿// 
+
+#pragma once
+
+#include "UI/LyraActivatableWidget.h"
+#include "MythicActivatableWidget.generated.h"
+
+class UCommonActivatableWidget;
+
+DECLARE_DYNAMIC_DELEGATE_OneParam(FInputActionExecutedDelegate, FName, ActionName);
+
+USTRUCT(BlueprintType)
+struct FInputActionBindingHandle
+{
+	GENERATED_BODY()
+	
+public:
+	FUIActionBindingHandle Handle;
+};
+
+/**
+ * Extends UCommonActivatableWidget with Blueprint-visible functions for registering additional input action bindings.
+ */
+UCLASS(Abstract, Blueprintable)
+class MYTHIC_API UMythicActivatableWidget : public UCommonActivatableWidget
+{
+    GENERATED_BODY()
+    
+protected:
+	virtual void NativeDestruct() override;
+
+	UFUNCTION(BlueprintCallable, Category = ExtendedActivatableWidget)
+	void RegisterBinding(FDataTableRowHandle InputAction, const FInputActionExecutedDelegate& Callback, FInputActionBindingHandle& BindingHandle);
+
+	UFUNCTION(BlueprintCallable, Category = ExtendedActivatableWidget)
+	void UnregisterBinding(FInputActionBindingHandle BindingHandle);
+
+	UFUNCTION(BlueprintCallable, Category = ExtendedActivatableWidget)
+	void UnregisterAllBindings();
+
+private:
+	TArray<FUIActionBindingHandle> BindingHandles;
+};
