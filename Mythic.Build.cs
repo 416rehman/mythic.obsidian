5f7f0e8058467d1279e63a1bf8b89ba14adf83ea
416rehman <416rehman@ahmadz.ai>
2025-09-04 23:27:10 -0400

    removed lua

--- Mythic.Build.cs
+++ Mythic.Build.cs
@@ -50,7 +50,6 @@ public class Mythic : ModuleRules
 			"OnlineSubsystemUtils",
 			"OnlineSubsystemEOS",
 			"NavigationSystem",
-			"LuaMachine",
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[]
